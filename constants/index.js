cf57edbe686323ff5899fc05c98764bfe8d40f01
Oluwatobi Akinrinade <tobi@dystopialabs.com>
2022-08-09 23:16:14 +0100

    client changes

---
+++ constants/index.js
@@ -0,0 +1,5 @@
+import LW3Punks from "../../../artifacts/contracts/LW3Punks.sol/LW3Punks.json";
+
+export const abi = LW3Punks.abi;
+export const NFT_CONTRACT_ADDRESS =
+	"0xd8D668ccE5569c0E519B1D59ed3e952699732872";
